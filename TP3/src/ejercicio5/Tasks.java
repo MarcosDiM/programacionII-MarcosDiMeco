1b7fe12bf076ff2e0c65ad3b3a07af25d1f34289
Marcos <marcosdimecom@gmail.com>
2024-04-30 09:42:12 -0300

    TP3

---
+++ TP3/src/ejercicio5/Tasks.java
@@ -0,0 +1,27 @@
+package ejercicio5;
+
+public class Tasks {
+    private String description;
+    private int priority;
+
+    public Tasks(String description, int priority) {
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarea{" +
+                "Descripcion= '" + description + '\'' +
+                ", Prioridad= " + priority +
+                '}';
+    }
+}
